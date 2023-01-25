9d2d2ab7d15763938f788abdfc2dd2e003ec9cd4
Said Azmour <azmour2016maroc@gmail.com>
2023-01-25 19:41:28 +0100

    create job database 1

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my_apps
+
+    'job',
+    'blog',
+    'home',
+    'contact',
 ]
 
 MIDDLEWARE = [
